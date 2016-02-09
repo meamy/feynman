ebc7d4971a526d9dc46043fa99279fe74a1d3ec7
Matt Amy <matt.e.amy@gmail.com>
2016-02-09 11:43:49 -0500

    Old .qc parser

--- Feyn.hs
+++ Feyn.hs
@@ -1,6 +1,14 @@
+module Main (main) where
+
 import System.Environment
+
+import Syntax
 
 main :: IO ()
 main = do
-  args <- getArgs
   putStrLn "Feyn -- copywrite 2016 Matthew Amy"
+  (f:xs) <- getArgs
+  s      <- readFile f
+  case parseQC s of
+    Left err -> putStrLn $ "Error parsing input: " ++ show err
+    Right circuit -> printCircIR circuit
