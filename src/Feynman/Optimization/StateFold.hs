0fb2654a3f7d37e6cecdaf3fe23b1a8a9497b86b
Matt Amy <matt.e.amy@gmail.com>
2020-05-13 16:59:18 -0300

    Small improvements to state folding

--- src/Feynman/Optimization/StateFold.hs
+++ src/Feynman/Optimization/StateFold.hs
@@ -23,12 +23,12 @@ import Feynman.Synthesis.Phase
    losing too much efficiency -}
 
 data Ctx = Ctx {
-  dim   :: Int,
-  ket   :: Map ID (F2Vec, Bool),
-  terms :: Map F2Vec (Set (Loc, Bool), Angle),
-  phase :: Angle,
-  pp    :: Multilinear Angle,
-  paths :: Set Int
+  dim   :: !Int,
+  ket   :: !(Map ID (F2Vec, Bool)),
+  terms :: !(Map F2Vec (Set (Loc, Bool), Angle)),
+  phase :: !Angle,
+  pp    :: !(Multilinear Angle),
+  paths :: !(Set Int)
 } deriving Show
 
 -- The /i/th variable
@@ -65,18 +65,32 @@ addTerm :: Angle -> Loc -> (F2Vec, Bool) -> State Ctx ()
 addTerm theta loc (bv, parity) = modify go where
   go st = case parity of
     False -> st { terms = Map.alter (add theta) bv (terms st),
-                  pp    = pp st + distribute theta (multilinearOfBV (bv, False)) }
+                  pp    = simplify $ pp st + poly }
     True  -> st { terms = Map.alter (add (-theta)) bv (terms st),
-                  pp    = pp st + distribute theta (multilinearOfBV (bv, True)),
+                  pp    = simplify $ pp st + poly,
                   phase = (phase st) + theta }
   add theta t = case t of
     Just (reps, theta') -> Just (Set.insert (loc, parity) reps, theta + theta')
     Nothing             -> Just (Set.singleton (loc, parity), theta)
+  poly = distribute theta (toPolynomial (bv, parity))
 
 -- Adds a quadratic phase term
 addQuadTerm :: Int -> (F2Vec, Bool) -> State Ctx ()
-addQuadTerm n bv = modify $ \st -> st { pp = pp st + ofVar (var n) * quad } where
-  quad = distribute (Discrete $ dyadic 1 1) (multilinearOfBV bv)
+addQuadTerm n bv = modify $ \st -> st { pp = simplify $ pp st + poly } where
+  poly = ofVar (var n) * distribute (Discrete $ dyadic 1 1) (toPolynomial bv)
+
+-- Finding linear [HH] reductions
+applyLinearReductions :: State Ctx ()
+applyLinearReductions = do
+  poly     <- gets pp
+  pathVars <- gets paths
+  outVars  <- gets (Set.unions . map (varsOfBV . fst) . Map.elems . ket)
+  case matchHHLinear poly (Set.difference pathVars outVars) pathVars of
+    Nothing         -> return ()
+    Just (x, y, bv) -> do
+      elimVar x
+      substVar y bv
+      applyLinearReductions
 
 -- Finding [HH] reductions
 applyReductions :: State Ctx ()
@@ -84,30 +98,44 @@ applyReductions = do
   poly     <- gets pp
   pathVars <- gets paths
   outVars  <- gets (Set.unions . map (varsOfBV . fst) . Map.elems . ket)
-  case matchHHLinear poly $ Set.difference pathVars outVars of
+  case matchHH poly (Set.difference pathVars outVars) pathVars of
     Nothing         -> return ()
-    Just (x, y, bv) -> do
+    Just (x, y, p) -> do
+      bv <- linearize p
       elimVar x
       substVar y bv
       applyReductions
 
 -- Remove an internal variable
 elimVar :: Int -> State Ctx ()
-elimVar x = modify $ \st -> st { terms = Map.filterWithKey f $ terms st,
-                                 pp    = removeVar (var x) $ pp st }
-  where f parity _ = Set.notMember x $ varsOfBV parity
+elimVar x = modify $ \st -> st { pp = simplify . removeVar (var x) $ pp st }
 
 -- Substitute a variable
 substVar :: Int -> (F2Vec, Bool) -> State Ctx ()
 substVar x bv = modify go where
   go st = st { terms = Map.mapKeysWith add substKey $ terms st,
-               pp    = P.subst (var x) (multilinearOfBV bv) $ pp st,
+               pp    = simplify $ P.subst (var x) (toPolynomial bv) (pp st),
                ket   = Map.map substState $ ket st }
   add (s1, a1) (s2, a2) = (Set.union s1 s2, a1 + a2)
   substKey bv' = case testBit bv' x of
     False -> bv'
     True  -> (complementBit bv' x) + (fst bv)
   substState bv = (substKey $ fst bv, snd bv)
+
+-- Converts a Boolean polynomial into a (symbolic) bitvector
+-- The idea is to package high degree terms into unique variables
+-- so that we maintain a linear presentation, later possibly
+-- subject to equalities
+linearize :: Multilinear Bool -> State Ctx (F2Vec, Bool)
+linearize = foldM go (bitI 0 0, False) . Map.keys . P.terms where
+  go (bv, parity) mono
+    | monomialDegree mono == 0 = return (bv, parity `xor` True)
+    | monomialDegree mono == 1 = do
+        let v = unvar $ firstVar mono
+        return (bv `xor` (bit v), parity)
+    | otherwise = do
+        n <- alloc
+        return (bv `xor` (bit n), parity)
 
 {- Utilities -}
 
@@ -118,11 +146,11 @@ injectZ2 a = case order a of
   _ -> Nothing
 
 toBooleanPoly :: (Eq a, Periodic a) => Multilinear a -> Maybe (Multilinear Bool)
-toBooleanPoly = convertMaybe injectZ2 . simplify
+toBooleanPoly = convertMaybe injectZ2
 
 -- Converts a bitvector into a polynomial
-multilinearOfBV :: (F2Vec, Bool) -> Multilinear Bool
-multilinearOfBV bv = Set.foldr (+) const . Set.map (ofVar . var) . varsOfBV $ fst bv
+toPolynomial :: (F2Vec, Bool) -> Multilinear Bool
+toPolynomial bv = Set.foldr (+) const . Set.map (ofVar . var) . varsOfBV $ fst bv
   where const = if snd bv then 1 else 0
 
 -- Gets the variables in a bitvector
@@ -130,8 +158,8 @@ varsOfBV :: F2Vec -> Set Int
 varsOfBV bv = Set.fromList $ filter (testBit bv) [0..(width bv) - 1]
 
 -- Converts a linear Boolean polynomial into a bitvector
-bvOfMultilinear :: Multilinear Bool -> Maybe (F2Vec, Bool)
-bvOfMultilinear p
+fromPolynomial :: Multilinear Bool -> Maybe (F2Vec, Bool)
+fromPolynomial p
   | degree p > 1 = Nothing
   | otherwise    = Just $ unsafeConvert p
     where unsafeConvert = (foldl' f (bitI 0 0, False)). Map.toList . P.terms
@@ -143,14 +171,23 @@ bvOfMultilinear p
                 (bv `xor` (bitI (v+1) v), const)
 
 -- Matches a *linear* instance of [HH]
-matchHHLinear :: Multilinear Angle -> Set Int -> Maybe (Int, Int, (F2Vec, Bool))
-matchHHLinear poly paths = msum . map (go . var) $ Set.toDescList paths where
+matchHHLinear :: Multilinear Angle -> Set Int -> Set Int -> Maybe (Int, Int, (F2Vec, Bool))
+matchHHLinear poly cand paths = msum . map (go . var) $ Set.toDescList cand where
   go v = do
-    p'       <- toBooleanPoly $ factorOut v poly
+    p'       <- toBooleanPoly . factorOut v $ poly
     (u, sub) <- find validSoln $ solveForX p'
-    bv       <- bvOfMultilinear sub
+    bv       <- fromPolynomial sub
     return (unvar v, unvar u, bv)
   validSoln (u, sub) = (unvar u) `elem` paths && degree sub <= 1
+
+-- Matches a instance of [HH]
+matchHH :: Multilinear Angle -> Set Int -> Set Int -> Maybe (Int, Int, Multilinear Bool)
+matchHH poly cand paths = msum . map (go . var) $ Set.toDescList cand where
+  go v = do
+    p'       <- toBooleanPoly . factorOut v $ poly
+    (u, sub) <- find validSoln $ solveForX p'
+    return (unvar v, unvar u, sub)
+  validSoln (u, sub) = (unvar u) `elem` paths
 
 {- The Super phase folding analysis -}
 applyGate :: (Primitive, Loc) -> State Ctx ()
@@ -184,42 +221,39 @@ applyGate (gate, l) = case gate of
 {- Run the analysis on a circuit and state -}
 runCircuit :: [Primitive] -> Ctx -> Ctx
 runCircuit circ = execState go where
-  go = mapM_ applyGate (zip circ [2..]) >> applyReductions
+  go = mapM_ applyGate (zip circ [2..]) >> applyLinearReductions
 
 {- Generates an initial state -}
 initialState :: [ID] -> [ID] -> Ctx
-initialState vars inputs = Ctx dim (Map.fromList ket) Map.empty 0 0 Set.empty where
+initialState vars inputs = st where
+  st  = Ctx dim (Map.fromList ket) Map.empty 0 0 Set.empty
   dim = length inputs
-  ket = zip (inputs ++ (vars \\ inputs)) [(bitI dim x, False) | x <- [0..]]
+  ket = zip (inputs ++ (vars\\inputs)) [(bitI dim x, False) | x <- [0..]]
   
 stateFold :: [ID] -> [ID] -> [Primitive] -> [Primitive]
 stateFold vars inputs circ =
   let result = runCircuit circ $ initialState vars inputs
-      (lmap, phase') =
-        let f (lmap, phase) (locs, exp) =
-              let (i, phase', exp') = case Set.findMin locs of
-                    (i, False) -> (i, phase, exp)
-                    (i, True)  -> (i, phase + exp, (-exp))
-              in
-                (Set.foldr (\(j, _) -> Map.insert j (if i == j then exp' else zero)) lmap locs, phase')
-        in
-          foldl' f (Map.empty, phase result) (Map.elems $ terms result)
-      circ' =
-        let getTarget gate = case gate of
-              T x    -> x
-              S x    -> x
-              Z x    -> x
-              Tinv x -> x
-              Sinv x -> x
-              Rz _ x -> x
-            f (gate, i) = case Map.lookup i lmap of
-              Nothing -> [(gate, i)]
-              Just exp
-                | exp == zero -> []
-                | otherwise   -> zip (synthesizePhase (getTarget gate) exp) (repeat i)
-        in
-          concatMap f (zip circ [2..])
+      phases = Map.elems $ terms result
+      (map, gphase) = foldr go (Map.empty, phase result) phases where
+        go (locs, angle) (map, gphase) =
+          let (loc, gphase', angle') = case Set.findMin locs of
+                (l, False) -> (l, gphase, angle)
+                (l, True)  -> (l, gphase + angle, (-angle))
+              update (l,_) = Map.insert l (if l == loc then angle' else 0)
+          in
+            (Set.foldr update map locs, gphase')
+      circ' = concatMap go (zip circ [2..]) where
+        go (gate, l) = case Map.lookup l map of
+              Nothing -> [(gate, l)]
+              Just angle
+                | angle == 0 -> []
+                | otherwise -> zip (synthesizePhase (getTarget gate) angle) [0..]
+        getTarget gate = case gate of
+          T x    -> x
+          S x    -> x
+          Z x    -> x
+          Tinv x -> x
+          Sinv x -> x
+          Rz _ x -> x
   in
-    (fst $ unzip $ circ') ++ (globalPhase (head vars) phase')
-
-  
+    (fst $ unzip $ circ') ++ (globalPhase (head vars) gphase)
