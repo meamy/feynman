c4170b667a34aeed39b3326b1bc3cac1cd0a7364
Matt Amy <matt.e.amy@gmail.com>
2020-05-14 11:07:51 -0300

    Replaced bitvectors with Boolean polynomials

--- src/Feynman/Optimization/StateFold.hs
+++ src/Feynman/Optimization/StateFold.hs
@@ -23,12 +23,12 @@ import Feynman.Synthesis.Phase
    losing too much efficiency -}
 
 data Ctx = Ctx {
-  dim   :: !Int,
-  ket   :: !(Map ID (F2Vec, Bool)),
-  terms :: !(Map F2Vec (Set (Loc, Bool), Angle)),
-  phase :: !Angle,
-  pp    :: !(Multilinear Angle),
-  paths :: !(Set Int)
+  dim   :: Int,
+  ket   :: Map ID (Multilinear Bool),
+  terms :: Map (Multilinear Bool) (Set (Loc, Bool), Angle),
+  phase :: Angle,
+  pp    :: Multilinear Angle,
+  paths :: Set Int
 } deriving Show
 
 -- The /i/th variable
@@ -47,49 +47,47 @@ alloc = do
   return n
 
 -- Get the state of variable v
-getSt :: ID -> State Ctx (F2Vec, Bool)
+getSt :: ID -> State Ctx (Multilinear Bool)
 getSt v = get >>= \st ->
   case Map.lookup v (ket st) of
-    Just bv -> return bv
+    Just bexp -> return bexp
     Nothing -> do
       n <- alloc
-      modify $ \st -> st { ket = Map.insert v (bit n, False) (ket st) }
-      return (bit n, False)
+      let bexp = ofVar $ var n
+      setSt v bexp
+      return bexp
 
 -- Set the state of a variable
-setSt :: ID -> (F2Vec, Bool) -> State Ctx ()
-setSt v bv = modify $ \st -> st { ket = Map.insert v bv (ket st) }
+setSt :: ID -> Multilinear Bool -> State Ctx ()
+setSt v bexp = modify $ \st -> st { ket = Map.insert v bexp (ket st) }
 
 -- Adds a mergeable phase term
-addTerm :: Angle -> Loc -> (F2Vec, Bool) -> State Ctx ()
-addTerm theta loc (bv, parity) = modify go where
-  go st = case parity of
-    False -> st { terms = Map.alter (add theta) bv (terms st),
-                  pp    = simplify $ pp st + poly }
-    True  -> st { terms = Map.alter (add (-theta)) bv (terms st),
-                  pp    = simplify $ pp st + poly,
-                  phase = (phase st) + theta }
+addTerm :: Angle -> Loc -> Multilinear Bool -> State Ctx ()
+addTerm theta loc bexp = modify go where
+  go st = st { terms = Map.alter (add theta) (dropConstant bexp) (terms st),
+               pp    = simplify $ pp st + distribute theta bexp,
+               phase = phase st + (if parity then theta else 0) }
   add theta t = case t of
     Just (reps, theta') -> Just (Set.insert (loc, parity) reps, theta + theta')
     Nothing             -> Just (Set.singleton (loc, parity), theta)
-  poly = distribute theta (toPolynomial (bv, parity))
+  parity = getConstant bexp
 
 -- Adds a quadratic phase term
-addQuadTerm :: Int -> (F2Vec, Bool) -> State Ctx ()
-addQuadTerm n bv = modify $ \st -> st { pp = simplify $ pp st + poly } where
-  poly = ofVar (var n) * distribute (Discrete $ dyadic 1 1) (toPolynomial bv)
+addQuadTerm :: Int -> Multilinear Bool -> State Ctx ()
+addQuadTerm n bexp = modify $ \st -> st { pp = simplify $ pp st + poly } where
+  poly = distribute (Discrete $ dyadic 1 1) $ ofVar (var n) * bexp
 
 -- Finding linear [HH] reductions
 applyLinearReductions :: State Ctx ()
 applyLinearReductions = do
   poly     <- gets pp
   pathVars <- gets paths
-  outVars  <- gets (Set.unions . map (varsOfBV . fst) . Map.elems . ket)
+  outVars  <- gets (Set.fromList . map unvar . concatMap vars . Map.elems . ket)
   case matchHHLinear poly (Set.difference pathVars outVars) pathVars of
     Nothing         -> return ()
-    Just (x, y, bv) -> do
+    Just (x, y, bexp) -> do
       elimVar x
-      substVar y bv
+      substVar y bexp
       applyLinearReductions
 
 -- Finding [HH] reductions
@@ -97,13 +95,12 @@ applyReductions :: State Ctx ()
 applyReductions = do
   poly     <- gets pp
   pathVars <- gets paths
-  outVars  <- gets (Set.unions . map (varsOfBV . fst) . Map.elems . ket)
+  outVars  <- gets (Set.fromList . map unvar . concatMap vars . Map.elems . ket)
   case matchHH poly (Set.difference pathVars outVars) pathVars of
     Nothing         -> return ()
-    Just (x, y, p) -> do
-      bv <- linearize p
+    Just (x, y, bexp) -> do
       elimVar x
-      substVar y bv
+      substVar y bexp
       applyReductions
 
 -- Remove an internal variable
@@ -111,31 +108,22 @@ elimVar :: Int -> State Ctx ()
 elimVar x = modify $ \st -> st { pp = simplify . removeVar (var x) $ pp st }
 
 -- Substitute a variable
-substVar :: Int -> (F2Vec, Bool) -> State Ctx ()
-substVar x bv = modify go where
-  go st = st { terms = Map.mapKeysWith add substKey $ terms st,
-               pp    = simplify $ P.subst (var x) (toPolynomial bv) (pp st),
-               ket   = Map.map substState $ ket st }
-  add (s1, a1) (s2, a2) = (Set.union s1 s2, a1 + a2)
-  substKey bv' = case testBit bv' x of
-    False -> bv'
-    True  -> (complementBit bv' x) + (fst bv)
-  substState bv = (substKey $ fst bv, snd bv)
-
--- Converts a Boolean polynomial into a (symbolic) bitvector
--- The idea is to package high degree terms into unique variables
--- so that we maintain a linear presentation, later possibly
--- subject to equalities
-linearize :: Multilinear Bool -> State Ctx (F2Vec, Bool)
-linearize = foldM go (bitI 0 0, False) . Map.keys . P.terms where
-  go (bv, parity) mono
-    | monomialDegree mono == 0 = return (bv, parity `xor` True)
-    | monomialDegree mono == 1 = do
-        let v = unvar $ firstVar mono
-        return (bv `xor` (bit v), parity)
-    | otherwise = do
-        n <- alloc
-        return (bv `xor` (bit n), parity)
+substVar :: Int -> Multilinear Bool -> State Ctx ()
+substVar x bexp = modify go where
+  go st = st { terms = Map.foldrWithKey f Map.empty $ terms st,
+               pp    = simplify . P.subst (var x) bexp $ pp st,
+               ket   = Map.map (P.subst (var x) bexp) $ ket st }
+  f sub val =
+    let bexp' = P.subst (var x) bexp sub
+        locs  = if getConstant bexp' then
+                  Set.map (\(locs,parity) -> (locs,Prelude.not parity)) $ fst val
+                else
+                  fst val
+        add (s1, a1) t = case t of
+          Just (s2, a2) -> Just (Set.union s1 s2, a1 + a2)
+          Nothing       -> Just (s1, a1)
+    in
+      Map.alter (add (locs, snd val)) (dropConstant bexp')
 
 {- Utilities -}
 
@@ -148,44 +136,21 @@ injectZ2 a = case order a of
 toBooleanPoly :: (Eq a, Periodic a) => Multilinear a -> Maybe (Multilinear Bool)
 toBooleanPoly = convertMaybe injectZ2
 
--- Converts a bitvector into a polynomial
-toPolynomial :: (F2Vec, Bool) -> Multilinear Bool
-toPolynomial bv = Set.foldr (+) const . Set.map (ofVar . var) . varsOfBV $ fst bv
-  where const = if snd bv then 1 else 0
-
--- Gets the variables in a bitvector
-varsOfBV :: F2Vec -> Set Int
-varsOfBV bv = Set.fromList $ filter (testBit bv) [0..(width bv) - 1]
-
--- Converts a linear Boolean polynomial into a bitvector
-fromPolynomial :: Multilinear Bool -> Maybe (F2Vec, Bool)
-fromPolynomial p
-  | degree p > 1 = Nothing
-  | otherwise    = Just $ unsafeConvert p
-    where unsafeConvert = (foldl' f (bitI 0 0, False)). Map.toList . P.terms
-          f (bv, const) (m, b)
-            | b == False      = (bv, const)
-            | emptyMonomial m = (bv, const `xor` True)
-            | otherwise       =
-              let v = unvar $ firstVar m in
-                (bv `xor` (bitI (v+1) v), const)
-
 -- Matches a *linear* instance of [HH]
-matchHHLinear :: Multilinear Angle -> Set Int -> Set Int -> Maybe (Int, Int, (F2Vec, Bool))
-matchHHLinear poly cand paths = msum . map (go . var) $ Set.toDescList cand where
+matchHHLinear :: Multilinear Angle -> Set Int -> Set Int -> Maybe (Int, Int, Multilinear Bool)
+matchHHLinear pp cand paths = msum . map (go . var) $ Set.toDescList cand where
   go v = do
-    p'       <- toBooleanPoly . factorOut v $ poly
-    (u, sub) <- find validSoln $ solveForX p'
-    bv       <- fromPolynomial sub
-    return (unvar v, unvar u, bv)
+    pp'      <- toBooleanPoly . factorOut v $ pp
+    (u, sub) <- find validSoln $ solveForX pp'
+    return (unvar v, unvar u, sub)
   validSoln (u, sub) = (unvar u) `elem` paths && degree sub <= 1
 
 -- Matches a instance of [HH]
 matchHH :: Multilinear Angle -> Set Int -> Set Int -> Maybe (Int, Int, Multilinear Bool)
-matchHH poly cand paths = msum . map (go . var) $ Set.toDescList cand where
+matchHH pp cand paths = msum . map (go . var) $ Set.toDescList cand where
   go v = do
-    p'       <- toBooleanPoly . factorOut v $ poly
-    (u, sub) <- find validSoln $ solveForX p'
+    pp'      <- toBooleanPoly . factorOut v $ pp
+    (u, sub) <- find validSoln $ solveForX pp'
     return (unvar v, unvar u, sub)
   validSoln (u, sub) = (unvar u) `elem` paths
 
@@ -199,23 +164,23 @@ applyGate (gate, l) = case gate of
   Z v    -> getSt v >>= addTerm (Discrete $ dyadic 1 1) l
   Rz p v -> getSt v >>= addTerm p l
   CNOT c t -> do
-    bv  <- getSt c
-    bv' <- getSt t
-    setSt t (fst bv + fst bv', snd bv `xor` snd bv')
-  X v      -> do
-    bv <- getSt v
-    setSt v (fst bv, Prelude.not $ snd bv)
-  H v      -> do
-    bv <- getSt v
+    bexp  <- getSt c
+    bexp' <- getSt t
+    setSt t (bexp + bexp')
+  X v -> do
+    bexp <- getSt v
+    setSt v (1 + bexp)
+  H v -> do
+    bexp <- getSt v
     n <- alloc
     modify $ \st -> st { paths = Set.insert n $ paths st }
-    addQuadTerm n bv
-    setSt v (bit n, False)
+    addQuadTerm n bexp
+    setSt v (ofVar $ var n)
   Swap u v -> do
-    bv  <- getSt u
-    bv' <- getSt v
-    setSt u bv'
-    setSt v bv
+    bexp  <- getSt u
+    bexp' <- getSt v
+    setSt u bexp'
+    setSt v bexp
   _      -> error "Unsupported gate"
 
 {- Run the analysis on a circuit and state -}
@@ -228,7 +193,7 @@ initialState :: [ID] -> [ID] -> Ctx
 initialState vars inputs = st where
   st  = Ctx dim (Map.fromList ket) Map.empty 0 0 Set.empty
   dim = length inputs
-  ket = zip (inputs ++ (vars\\inputs)) [(bitI dim x, False) | x <- [0..]]
+  ket = (zip inputs [ofVar (var x) | x <- [0..]]) ++ (zip (vars\\inputs) $ repeat 0)
   
 stateFold :: [ID] -> [ID] -> [Primitive] -> [Primitive]
 stateFold vars inputs circ =
