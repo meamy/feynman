611d913b7e86a6aa12bdc341e1e023e215ed6dc0
Kevin Ye <kevinye88@gmail.com>
2024-07-11 17:30:03 -0700

    fixed new implementation of control, added controlledN for multiply controlled version

--- src/Feynman/Algebra/Pathsum/Balanced.hs
+++ src/Feynman/Algebra/Pathsum/Balanced.hs
@@ -701,14 +701,50 @@ controlled sop@(Pathsum a b c d e f) = Pathsum a (b+1) (c+1) d e' f' where
 
 -- attempt at making controlled work with hgate
 controlled' :: (Eq g, Abelian g, Dyadic g) => Pathsum g -> Pathsum g
-controlled' sop@(Pathsum a b c d e f) = Pathsum n (b+1) (c+1) n e' f' where
+controlled' sop = Pathsum a (b+1) (c+1) d e' f' where
+  Pathsum a b c d e f = until balanced balancePathsum sop
   shift   = shiftI 1
   x       = ofVar $ IVar 0
-  n       = max a d
-  e'      = (lift x)*(renameMonotonic shift e) + distribute (-(fromInteger . toInteger $ n)*half*half) (1 + x)
-            +  distribute half (lift $ (1+x) * (foldr (+) 0 . map (\i -> ofVar $ PVar i) $ [0..n-1]))
+  e'      = (lift x)*(renameMonotonic shift e) + (foldr (+) 0 . replicate a $ (distribute (-half*half) (1 + x)))
+            + (foldr (+) 0 . map (\i -> distribute half . lift $ (1+x) * (ofVar $ PVar i) ) $ [0..a-1])
   f'      = [lift x] ++ (map g . zip [1..] . map (renameMonotonic shift) $ f)
-  g (i,y) = (ofVar $ IVar i) + x*((ofVar $ IVar i) + y)  
+  g (i,y) = (ofVar $ IVar i) + x*((ofVar $ IVar i) + y)
+
+  balanced (Pathsum a _ _ d _ _) = a == d
+  balancePathsum sop@(Pathsum a _ _ d _ _)
+    | a < d  = sop <> oneA
+    | a > d  = sop <> oneD
+    | a == d = sop
+  oneA = Pathsum 2 0 0 1 0 []
+  oneD = Pathsum 1 0 0 2 ( distribute (half * half) (ofVar $ PVar 0)
+                         + distribute (half * half) (ofVar $ PVar 0) 
+                         + distribute (half * half) (ofVar $ PVar 0) 
+                         + distribute (half * half) (ofVar $ PVar 1) 
+                         + (constant (-half))) []
+
+controlledN :: (Eq g, Abelian g, Dyadic g) => Int -> Pathsum g -> Pathsum g
+controlledN n sop = Pathsum a (b+n) (c+n) d e' f' where
+  Pathsum a b c d e f = until balanced balancePathsum sop
+  shift               = shiftI n
+  xs                  = map (\i -> ofVar $ IVar i) $ [0..n-1]
+  prodxs              = foldr (*) 1 xs
+  e'                  = (lift prodxs) * (renameMonotonic shift e) 
+                        + (foldr (+) 0 . replicate a $ (distribute (-half*half) (1 + prodxs)))
+                        + (foldr (+) 0 . map (\i -> distribute half . lift $ (1 + prodxs) * (ofVar $ PVar i) ) $ [0..a-1])
+  f'                  = map lift xs ++ (map g . zip [n..] . map (renameMonotonic shift) $ f)
+  g (i,y)             = (ofVar $ IVar i) + prodxs*((ofVar $ IVar i) + y)
+
+  balanced (Pathsum a _ _ d _ _) = a == d
+  balancePathsum sop@(Pathsum a _ _ d _ _)
+    | a < d  = sop <> oneA
+    | a > d  = sop <> oneD
+    | a == d = sop
+  oneA = Pathsum 2 0 0 1 0 []
+  oneD = Pathsum 1 0 0 2 ( distribute (half * half) (ofVar $ PVar 0)
+                         + distribute (half * half) (ofVar $ PVar 0)
+                         + distribute (half * half) (ofVar $ PVar 0)
+                         + distribute (half * half) (ofVar $ PVar 1)
+                         + (constant (-half))) []
 
 -- | Attempt to add two path sums. Only succeeds if the resulting sum is balanced
 --   and the dimensions match.
