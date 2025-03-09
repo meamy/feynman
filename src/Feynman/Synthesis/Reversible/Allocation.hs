7b24c8795e202a7ca29f509552f1f667e0a326a6
Joseph Lunderville <jlunderv@sfu.ca>
2025-03-08 23:33:15 -0800

    More work on compute/uncompute algorithm

--- src/Feynman/Synthesis/Reversible/Allocation.hs
+++ src/Feynman/Synthesis/Reversible/Allocation.hs
@@ -1,8 +1,13 @@
 module Feynman.Synthesis.Reversible.Allocation where
 
-import Data.IntMap.Strict (IntMap, (!))
-import Data.IntMap.Strict qualified as IntMap
+import Data.Set (Set)
+import Data.Set qualified as Set
+import Data.IntMultiSet (IntMultiSet, (\\))
+import Data.IntMultiSet qualified as IntMultiSet
+import Data.MultiMap (MultiMap)
+import Data.MultiMap qualified as MultiMap
 import Feynman.Synthesis.XAG.Graph qualified as XAG
+import Data.Maybe (fromMaybe)
 
 newtype Computation = C Int deriving (Eq, Ord, Show)
 
@@ -20,24 +25,56 @@ reverseComputation (C cid) = C (-cid)
 
 isComputationReversed (C cid) = cid < 0
 
+newtype ComputedResultBag = CRB IntMultiSet deriving (Eq, Ord, Show)
+
 data AllocationProblem = AllocationProblem
-  { computations :: [(Computation, [ComputedResult], [ComputedResult])],
-    requiredResults :: [ComputedResult],
-    permittedResults :: [ComputedResult],
-    initialResults :: [ComputedResult]
+  { computations :: MultiMap Computation (ComputedResultBag, ComputedResultBag),
+    requiredResults :: ComputedResultBag,
+    permittedResults :: ComputedResultBag,
+    initialResults :: ComputedResultBag
   }
 
-newtype ComputationState = CS (IntMap Int)
+newtype ComputationState = CS IntMultiSet
 
-isComputed :: ComputedResult -> ComputationState -> Bool
-isComputed (CR resI) (CS stateM) = IntMap.member resI stateM
+unC (C i) = i
+unCR (CR i) = i
+unCRB (CRB ms) = ms
+unCS (CS ms) = ms
+
+emptyResults = CRB IntMultiSet.empty
+
+resultsAsSet :: ComputedResultBag -> Set ComputedResult
+resultsAsSet (CRB bag) = Set.fromList (map CR (IntMultiSet.elems bag))
+
+missingFrom :: ComputationState -> ComputedResultBag -> ComputedResultBag
+missingFrom (CS has) (CRB needs) = CRB (needs \\ has)
+
+alreadyHave :: ComputationState -> ComputedResultBag -> ComputedResultBag
+alreadyHave (CS has) (CRB needs) = CRB (IntMultiSet.intersection has needs)
+
+inBoth :: ComputedResultBag -> ComputedResultBag -> ComputedResultBag
+inBoth (CRB aBag) (CRB bBag) = CRB (IntMultiSet.intersection aBag bBag)
+
+afterApplication :: ComputationState -> ComputedResultBag -> ComputedResultBag -> ComputedResultBag
+afterApplication (CS has) (CRB needs) (CRB yields) = CRB ((has \\ needs) `IntMultiSet.union` yields)
+
+canApply :: ComputationState -> ComputedResultBag -> Bool
+canApply (CS has) (CRB needs) = (needs \\ has) == IntMultiSet.empty
+
+resultBagFromList :: [ComputedResult] -> ComputedResultBag
+resultBagFromList = CRB . IntMultiSet.fromList . map unCR
+
+resultBagToList :: ComputedResultBag -> [ComputedResult]
+resultBagToList = map CR . IntMultiSet.toList . unCRB
+
+allComputed :: ComputedResultBag -> ComputationState -> Bool
+allComputed (CRB wants) (CS has) = has `IntMultiSet.isSubsetOf` wants
+
+computedCount :: ComputedResult -> ComputationState -> Int
+computedCount (CR check) (CS has) = IntMultiSet.occur check has
 
 addComputed :: ComputedResult -> ComputationState -> ComputationState
-addComputed (CR resI) (CS stateM) = CS (IntMap.insertWith (+) resI 1 stateM)
+addComputed (CR got) (CS has) = CS (IntMultiSet.insert got has)
 
 removeComputed :: ComputedResult -> ComputationState -> ComputationState
-removeComputed (CR resI) (CS stateM) =
-  let updateF i
-        | i > 1 = Just (i - 1)
-        | otherwise = Nothing
-   in CS (IntMap.update updateF resI stateM)
+removeComputed (CR lost) (CS has) =CS (IntMultiSet.delete lost has)
--- src/Feynman/Synthesis/Reversible/Allocation/ComputeFirst.hs
+++ src/Feynman/Synthesis/Reversible/Allocation/ComputeFirst.hs
@@ -3,45 +3,94 @@ module Feynman.Synthesis.Reversible.Allocation.ComputeFirst
   )
 where
 
+import Control.Monad (foldM)
+import Control.Monad.State.Strict
 import Data.Foldable (foldl')
 import Data.Map.Strict (Map, (!))
 import Data.Map.Strict qualified as Map
+import Data.MultiMap qualified as MultiMap
+import Data.Set (Set, (\\))
+import Data.Set qualified as Set
 import Feynman.Synthesis.Reversible.Allocation
 
-computeFirstAllocate :: AllocationProblem -> [Computation]
+-- Conditions required by this allocation strategy:
+-- 1. there must be computations in the problem sufficient to meet the
+--    requirements the copyCs and singleCs in the CFS
+-- 2. all the initialResults must be in the permittedResults
+
+computeFirstAllocate :: AllocationProblem -> Maybe [Computation]
 computeFirstAllocate = undefined
+
+-- ComputeFirst, because first we compute everything we want, then we
+-- uncompute the things we don't want.
 
 -- Our specification leads to a slightly relaxed game compared to what's
 -- generally described as the reversible pebbling game in the literature, but
 -- I think it still give us a correct algorithm.
 
 -- copyCs and singleCs are expected to be complete, meaning:
--- - copyCs should have an entry for every possible computation
+-- - copyCs should have an entry for every result BESIDES the ones in the
+--   problem's initialResults
 -- - singleCs should be derived from a computation DAG, and given unbounded
 --   ancillas it should be possible to compute everything in the graph by
 --   going through it in topological order (with the caveat that some
 --   computation steps might need to be repeated where previous results get
 --   consumed by later steps)
-data ComputeFirstSpecification
-  = CFS
+data ComputeFirstProblem
+  = CFP
   { problem :: AllocationProblem,
     -- copyCs are computations that duplicate exactly one specific result, and
-    -- consume ancillas with no other dependencies
-    copyCs :: Map ComputedResult [(Computation, [ComputedResult], [ComputedResult])],
+    -- consume ancillas with no other dependencies; note that inverted, these
+    -- can uncopy and return extra outputs to 0
+    copyCs :: Map ComputedResult Computation,
     -- singleCs are computations that produce exactly one specific new result,
     -- but may consume anything
-    singleCs :: Map ComputedResult [(Computation, [ComputedResult], [ComputedResult])]
+    singleCs :: Map ComputedResult Computation
   }
 
-ensureComputed :: ComputeFirstSpecification -> ComputedResult -> ComputationState -> [Computation] -> (ComputationState, [Computation])
-ensureComputed spec res state stepsRev
-  -- already computed?
-  | isComputed res state = (state, stepsRev)
-  -- compute
-  | otherwise = foldl' (\(a, b) c -> ensureComputed spec c a b) (state, stepsRev) []
-  where
-    p = problem spec
-    fwd = singleCs spec ! res :: [(Computation, [ComputedResult], [ComputedResult])]
+data ComputeFirstState
+  = CFS
+  { cmptState :: ComputationState,
+    stepsRev :: [Computation]
+  }
 
-ensureUncomputed :: ComputeFirstSpecification -> ComputationState -> ComputedResult -> [Computation] -> [Computation]
+addStep :: Computation -> ComputeFirstState -> Maybe ComputeFirstState
+addStep c s@(CFS {stepsRev = sr}) = Just (s {stepsRev = c : sr})
+
+ensureComputed :: ComputeFirstProblem -> ComputedResultBag -> ComputedResult -> ComputeFirstState -> Maybe ComputeFirstState
+ensureComputed cfprob wantKeep res st
+  -- already computed?
+  | computedCount res (cmptState st) > 0 = return st
+  -- try computing each dependency
+  | unmetNeeds == emptyResults = computeResult
+  | otherwise = ensureDependenciesAndRetry
+  where
+    -- The computation that gets us res
+    cmpt = singleCs cfprob ! res
+    (needs, yields) = head (computations (problem cfprob) MultiMap.! cmpt)
+    unmetNeeds = missingFrom (cmptState st) needs
+
+    computeResult = do
+      -- insert dups to satisfy needs counts
+      foldM (\s r -> addStep (copyCs cfprob ! r) s) st wantAndWillLose
+      -- insert dups for wantAndWillLose
+      -- insert computation
+      -- insert annihilates to reduce counts not in wants
+      return st
+      where
+        -- things we have, that we want to keep, that we will lose -- to be dup'd
+        -- we use sets because the multiplicity isn't important, in fact we would
+        -- prefer to duplicate things just before they're actually needed to
+        -- minimize qubit use
+        wantAndWillLose = Set.toList (wantAndHave Set.\\ willHave)
+        willHave = resultsAsSet (afterApplication (cmptState st) needs yields)
+        wantAndHave = resultsAsSet (alreadyHave (cmptState st) wantKeep)
+
+    ensureDependenciesAndRetry =
+      foldM (flip (ensureComputed cfprob depWants)) st []
+        >>= ensureComputed cfprob wantKeep res
+      where
+        depWants = wantKeep
+
+ensureUncomputed :: ComputeFirstProblem -> ComputationState -> ComputedResult -> [Computation] -> [Computation]
 ensureUncomputed = undefined
