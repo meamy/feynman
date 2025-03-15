0698e6f1a48e8ed1569264df63a16a19392568c1
Matt Amy <matt.e.amy@gmail.com>
2025-03-15 15:00:15 -0700

    Working example

--- src/Feynman/Optimization/SSO.hs
+++ src/Feynman/Optimization/SSO.hs
@@ -19,9 +19,13 @@ import Data.Bits
 import Data.Foldable
 import Data.Map (Map, (!))
 import qualified Data.Map as Map
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Feynman.Core hiding (dagger)
 import Feynman.Algebra.Base
+import Feynman.Algebra.Polynomial
+import Feynman.Algebra.Polynomial.Multilinear
 import Feynman.Algebra.Pathsum.Balanced
 import Feynman.Verification.Symbolic
 
@@ -41,17 +45,26 @@ type Ctx = Map ID (Pathsum DMod2)
 
 -- | Top-level optimization
 sso :: Circuit -> Circuit
-sso circ = circ { decls = decls' } where
-  decls' = decls circ
+sso circ =
+  let (gates,[main])       = break (\decl -> name decl == "main") $ decls circ
+      processDecl ctx decl = Map.insert (name decl) (simulateDecl ctx decl) ctx
+      ctx                  = foldl' processDecl Map.empty gates
+      main'                = optimize ctx (qubits circ) (inputs circ) (body main)
+  in
+    circ { decls = gates ++ [main { body = main' }] }
+
+-- | Turns a symbolic execution into an operator
+operatorize :: [ID] -> Pathsum DMod2 -> Pathsum DMod2
+operatorize xs = bind (reverse xs)
 
 -- | simulates a declaration
 simulateDecl :: Ctx -> Decl -> Pathsum DMod2
-simulateDecl ctx (Decl _ params body) = go initPS body where
+simulateDecl ctx (Decl _ params body) = operatorize params $ go initPS body where
   n        = length params
   
   paramMap = Map.fromList $ zip params [0..]
 
-  initPS = identity (length params)
+  initPS = ket [ofVar v | v <- params]
 
   go ps stmt = grind $ case stmt of
     Gate g      -> evalState (applyPrimitive g ps) paramMap
@@ -66,7 +79,7 @@ simulateDecl ctx (Decl _ params body) = go initPS body where
         ps .> psStmt
 
   summarizeLoop i stmt =
-    let psStmt  = go initPS stmt
+    let psStmt  = operatorize params $ go initPS stmt
         squares = let f x = x : f (grind $ x .> x) in f psStmt
         acc j i ps = case i <= 0 of
           True  -> ps
@@ -79,3 +92,98 @@ simulateDecl ctx (Decl _ params body) = go initPS body where
 -- | Checks whether the miter of two path sums is the identity
 equal :: Pathsum DMod2 -> Pathsum DMod2 -> Bool
 equal ps ps' = isTrivial (grind $ ps .> dagger ps')
+
+-- | Optimizes a declaration
+optimizeDecl :: Ctx -> Decl -> (Decl, Pathsum DMod2)
+optimizeDecl ctx (Decl name params body) = (Decl name params body', operatorize params ps) where
+  n        = length params
+  
+  paramMap = Map.fromList $ zip params [0..]
+
+  initPS = ket [ofVar v | v <- params]
+
+  (body', ps) = runState (go body) initPS
+
+  go stmt = (\a -> modify grind >> return a) =<< case stmt of
+    Gate g      -> do
+      ps <- get
+      let ps'  = evalState (applyPrimitive g ps) paramMap
+      put ps'
+      if equal ps ps' then return (Gate (Uninterp "skip" [])) else return stmt
+
+    Seq xs      -> fmap Seq $ mapM go xs
+
+    Call f args ->
+      let embedF = \i -> paramMap!(args!!i)
+          psF    = embed (ctx!f) (n - length args) embedF embedF
+      in do
+        ps <- get
+        let ps' = ps .> psF
+        put ps'
+        if equal ps ps' then return (Gate (Uninterp "skip" [])) else return stmt
+
+    Repeat i stmt ->
+      let psStmt = summarizeLoop i stmt in do
+        modify (\ps -> ps .> psStmt)
+        return stmt
+
+  summarizeLoop i stmt =
+    let psStmt  = operatorize params $ execState (go stmt) initPS
+        squares = let f x = x : f (grind $ x .> x) in f psStmt
+        acc j i ps = case i <= 0 of
+          True  -> ps
+          False ->
+            let ps' = if i `mod` 2 == 1 then ps .> squares!!j else ps in
+              acc (j+1) (i `shiftR` 1) ps'
+    in
+      acc 0 i initPS
+  
+
+-- | Optimizes a statment
+optimize :: Ctx -> [ID] -> Set ID -> Stmt -> Stmt
+optimize ctx qubits inputs stmt = evalState (go stmt) initPS where
+  n        = length qubits
+  
+  qubitMap = Map.fromList $ zip qubits [0..]
+
+  initPS = ket [if v `elem` inputs then ofVar v else 0 | v <- qubits]
+
+  go stmt = (\a -> modify grind >> return a) =<< case stmt of
+    Gate g      -> do
+      ps <- get
+      let ps'  = evalState (applyPrimitive g ps) qubitMap
+      put ps'
+      if equal ps ps' then return (Gate (Uninterp "skip" [])) else return stmt
+
+    Seq xs      -> fmap Seq $ mapM go xs
+
+    Call f args ->
+      let embedF = \i -> qubitMap!(args!!i)
+          psF    = embed (ctx!f) (n - length args) embedF embedF
+      in do
+        ps <- get
+        let ps' = ps .> psF
+        put ps'
+        if equal ps ps' then return (Gate (Uninterp "skip" [])) else return stmt
+
+    Repeat i stmt -> go (Seq $ replicate i stmt)
+
+-- | Tests
+
+
+toffoliRem = Circuit { qubits = ["x", "y", "z"],
+                       inputs = Set.fromList ["z"],
+                       decls  = [tof,main] }
+    where tof = Decl { name = "tof",
+                       params = ["x", "y", "z",
+                       body = Seq [ Gate $ H "z",
+                                    Gate $ T "x", Gate $ T "y", Gate $ T "z", 
+                                    Gate $ CNOT "x" "y", Gate $ CNOT "y" "z", Gate $ CNOT "z" "x",
+                                    Gate $ Tinv "x", Gate $ Tinv "y", Gate $ T "z",
+                                    Gate $ CNOT "y" "x",
+                                    Gate $ Tinv "x",
+                                    Gate $ CNOT "y" "z", Gate  $ CNOT "z" "x", Gate $ CNOT "x" "y",
+                                    Gate $ H "z" ] }
+          main = Decl { name = "main",
+                        params = [],
+                        body = Seq [ Call "tof" ["x", "y", "z"] ] }
