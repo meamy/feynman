bd6484952f1d8ded98c0ae145d7cbc58bf85903f
Matt Amy <matt.e.amy@gmail.com>
2025-05-10 15:07:07 -0700

    Grammar for specifications

--- src/Feynman/Frontend/OpenQASM3/Spec.hs
+++ src/Feynman/Frontend/OpenQASM3/Spec.hs
@@ -20,36 +20,27 @@ data BOp = Plus | Minus | Times | Div | Mod
          | And | Xor | Or | LShift | RShift | LRot | RRot
          deriving (Show)
 
-data UOp = Neg | Wt deriving (Show)
+data UOp = Neg | Wt | Exp | Sqrt deriving (Show)
 
 -- | Classical types which can be in superposition
 data Type = Bit | ZN Int deriving Show
 
--- | Classical sub-language
-data CExp = CVar ID
-          | TT
-          | FF
-          | ILit Int
-          | BExp CExp BOp CExp
-          | UExp UOp CExp
-          deriving (Show)
-
--- | Scalar sub-language. Only phases can depend on path indices
-data RExp = CondPhase Angle CExp
-          | Norm DyadicRational
-          | Mult RExp RExp
-          deriving (Show)
-
--- | Matrix-valued terms
-data SOP = Segment [ID]
-         | Ket CExp
-         | Scalar RExp
-         | Fun ID Type SOP
-         | Sum ID Type SOP
-         | Tensor SOP SOP
-         | Compose SOP SOP
-         | Dagger SOP
-         deriving (Show)
+-- | Sum-over-path expressions
+data SExpr = Var ID (Maybe Int)
+           | BLit Bool
+           | ILit Int
+           | RLit Double
+           | Pi
+           | BExp SExpr BOp SExpr
+           | UExp Uop SExpr
+           -- Sum terms
+           | Ket SExpr
+           | Fun ID Type SExpr
+           | Sum ID Type SExpr
+           | Comma SExpr SExpr
+           | Compose SExpr SExpr
+           | Dagger SExpr
+           deriving (Show)
 
 -- | Assertions. Conjunctions of assertions are represented as lists
 data Assertion = Equals SOP SOP
@@ -57,5 +48,80 @@ data Assertion = Equals SOP SOP
 {- Semantic checks -}
 
 {- Parsing -}
+{--
+
+ Abstract
+
+  <expr> := 0 | 1 | <nat> | <real> | <var>([<nat>])?
+          | <uop> <expr>
+          | <expr> <uop> <expr>
+          | ( <expr> )
+          | | <expr> >
+          | fun <var>(:<type>)? -> <expr>
+          | sum <var>(:<type>)? . <expr>
+          | <expr> , <expr>
+          | <expr> <expr>
+          | <expr>`
+
+  <uop>  := ! | ~ | - | popcount | exp | sqrt
+  <bop>  := + | - | * | / | % | | << | >> | <<< | >>>
+  <type> := bool | bit[<expr>] | uint[<expr>]
+
+  Concrete
+
+  <type> := bool | bit[<expr>] | uint[<expr>]
+
+  <specification> := <exprs> --> <exprs>
+                   | <assertions>
+ 
+  <assertions> := <assertion>
+                | <assertions> && <assertion>
+
+  <assertion> := <exprs> == <exprs>
+
+  <exprs> := <expr>
+           | <exprs> , <expr>
+
+  <expr> := <term>
+          | <expr> + <term>
+          | <expr> - <term>
+          | <expr> % <term>
+
+  <term> := <factor>
+          | <term> * <factor>
+          | <term> / <factor>
+
+  <factor> := <atom>
+            | <factor> << <atom>
+            | <factor> <<< <atom>
+            | <factor> >> <atom>
+            | <factor> >>> <atom>
+
+  <atom> := <bool>
+          | <nat>
+          | <real>
+          | pi
+          | <id>
+          | <id> [ <expr> ]
+          | ( expr )
+          | | <exprs> >
+          | < <exprs> |
+          | fun <decls> <var>(:<type>)? -> <expr>
+          | sum <decls> <var>(:<type>)? . <expr>
+          | <atom>`
+          | ! <atom>
+          | ~ <atom>
+          | - <atom>
+          | <unary> ( <expr> )
+
+  <decls> := <decl>
+           | <decl> , <decls>
+
+  <decl> := <var>
+          | <var> : <type>
+
+  <unary> := exp | popcount | sqrt
+
+--}
 
 {- Translation to Balanced Pathsums -}
